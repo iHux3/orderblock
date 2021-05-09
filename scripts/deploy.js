9b7acb5972160c3a751a4c647589192afa497a44
Jakub <ihux3x@gmail.com>
2021-05-09 18:43:28 +0200

    deploy changed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,8 +12,14 @@ async function main() {
     await testToken1.approve(orderBlock.address, web3.utils.toWei('10', 'ether'));
     await orderBlock.createOrder(1, web3.utils.toWei('0.5', 'ether'), value, 0, 0, 0);
     await orderBlock.createOrder(1, web3.utils.toWei('1', 'ether'), value, 0, 0, 0);
+    await orderBlock.createOrder(1, web3.utils.toWei('1', 'ether'), value, 0, 0, 0);
     await orderBlock.createOrder(1, web3.utils.toWei('1.5', 'ether'), value, 1, 0, 0, {value: value});
     await orderBlock.createOrder(1, web3.utils.toWei('2', 'ether'), value, 1, 0, 0, {value: value});
+
+    await orderBlock.createOrder(1, web3.utils.toWei('0.25', 'ether'), value, 1, 1, 0, {value: web3.utils.toWei('1.005', 'ether')});
+    await orderBlock.createOrder(1, web3.utils.toWei('1', 'ether'), value, 1, 1, 0, {value: web3.utils.toWei('1.005', 'ether')});
+    await orderBlock.createOrder(1, web3.utils.toWei('1.5', 'ether'), value, 0, 1, 0, {value: web3.utils.toWei('0.005', 'ether')});
+    await orderBlock.createOrder(1, web3.utils.toWei('2.5', 'ether'), value, 0, 1, 0, {value: web3.utils.toWei('0.005', 'ether')});
   
     console.log("OrderBlock deployed to:", orderBlock.address);
     console.log("Token deployed to:", testToken1.address);
